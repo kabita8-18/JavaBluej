95b192e0b19265698bf0350927d2aef7a1392aab
Kabita Khadka <kabitakhadka713@gmail.com>
2025-11-21 13:02:59 +0545

    Initial sharing of project

---
+++ College.java
@@ -0,0 +1,10 @@
+
+/**
+ * Write a description of class College here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class College
+{
+}
---
+++ Tutorial3.java
@@ -0,0 +1,73 @@
+
+/**
+ * Write a description of class Tutorial3 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Tutorial3
+{
+    
+    int agr;// instance variable
+    static int rtr; // static variable
+    
+    public static void main(String[] args);
+    int age=18; // local variable
+    System.out.println<(age)>;
+    
+    
+    // <className> <variable> =new <className>();
+    
+    Tutorail3 t1= new Tutorail3();
+    
+    System.out.println(rtr);
+    System.out.println(t1.agr);
+    
+    // Implicit Typecasting
+    
+    double db= age;
+    
+    System.out.println(db);
+    
+    
+    
+    // Explicit Typecasting
+    
+    
+    double dbr= 10.90;
+    int itr= (itr)dbr;
+    
+    System.out.println(itr);
+    
+    
+// Exceptions
+
+short st= 10;
+short str= 11;
+
+short sum= (short)(st + str);
+
+//Maximum, minimum, bits, bytes
+
+
+System.out.println(Byte.MAX_VALUE); // maximum value
+ 
+
+
+
+
+
+
+
+// Escape Sequence
+
+System.out.println("Hello\nWorld"); // for new line
+System.out.println("Hamro\tNepal") // for tabs
+System.out.println("She 
+
+ 
+
+
+     
+    
+}
